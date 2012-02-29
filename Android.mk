906c630c8f43618456c59918fb92f6b282131cc5
LorD ClockaN <davor@losinj.com>
2012-02-29 15:33:05 +0100

    fix build

--- Android.mk
+++ Android.mk
@@ -5,8 +5,8 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
 endif
-ifeq ($(BOARD_USES_QCOM_LIBS),true)
-	include $(TOP)/vendor/qcom/opensource/omx/mm-core/Android.mk
-	include $(TOP)/vendor/qcom/opensource/omx/mm-video/Android.mk
-endif
+#ifeq ($(BOARD_USES_QCOM_LIBS),true)
+#	include $(TOP)/vendor/qcom/opensource/omx/mm-core/Android.mk
+#	include $(TOP)/vendor/qcom/opensource/omx/mm-video/Android.mk
+#endif
 endif
